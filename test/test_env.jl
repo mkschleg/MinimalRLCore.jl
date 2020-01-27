3fb672b7a06c12f27245c3b429a93c044f563d00
Matthew Schlegel <mkschleg@gmail.com>
2020-01-27 13:11:20 -0700

    Updating API for easier use by users.

--- test/test_env.jl
+++ test/test_env.jl
@@ -13,27 +13,37 @@ end
 
 name(t::TestEnv) = t.name
 
-RLCore.reset!(env::TestEnv, rng::Random.AbstractRNG) = env.state = rand(rng) * env.size
 RLCore.get_state(env::TestEnv) = env.state
 RLCore.get_reward(env::TestEnv) = env.rew
 RLCore.get_actions(env::TestEnv) = (1, 2)
 RLCore.is_terminal(env::TestEnv) = false
 
-function RLCore.environment_step!(env::TestEnv,
-                                  action,
-                                  rng::AbstractRNG; kwargs...)
+
+RLCore.reset!(env::TestEnv, rng::Random.AbstractRNG=Random.GLOBAL_RNG) =
+    env.state = rand(rng) * env.size
+
+RLCore.environment_step!(env::TestEnv, action, rng::AbstractRNG=Random.GLOBAL_RNG) = 
     env.rew = action*env.state
-    return
-end
+
 
 function test_env()
-    rng = MersenneTwister(10)
+    Random.seed!(10)
     expected_state = 0.5629122239323647
-    @testset "TestEnv" begin
+    @testset "TestEnv Global RNG" begin
         env = TestEnv("Matt", 5)
-        @test start!(env, rng) ≈ expected_state
+        @test start!(env) ≈ expected_state
         @test all(step!(env, 1) .≈ (expected_state, 1*expected_state, false))
         @test all(step!(env, 2) .≈ (expected_state, 2*expected_state, false))
+        @test all(RLCore.get_actions(env) .== (1,2))
+    end
+
+    
+    rng = MersenneTwister(10)
+    @testset "TestEnv Managed RNG" begin
+        env = TestEnv("Matt", 5)
+        @test start!(env, rng) ≈ expected_state
+        @test all(step!(env, 1, rng) .≈ (expected_state, 1*expected_state, false))
+        @test all(step!(env, 2, rng) .≈ (expected_state, 2*expected_state, false))
         @test all(RLCore.get_actions(env) .== (1,2))
     end
 end
