95151418a288f2c1d083a90b95804f5f21e07c13
Matthew Schlegel <mkschleg@gmail.com>
2020-04-24 15:17:54 -0600

    Merging.

--- src/MinimalRLCore.jl
+++ src/MinimalRLCore.jl
@@ -24,29 +24,12 @@ export
     run_episode!
 include("episode.jl")
 
-# export
-#     GVF,
-#     Horde,
-#     GVFParamFuncs
-# include("gvf.jl")
-
 export
     AbstractFeatureConstructors,
     create_features,
     feature_size
 include("feature_constructors.jl")
 
-
-# export
-#     TileCoder,
-#     MinMaxNormalize,
-#     MeanStdNormalize
-# include("features/TileCoder.jl")
-# include("features/Normalize.jl")
-
-# export
-#     RandomAgent
-# include("agent/random.jl")
 
 # Not tested yet so not exported!
 # include("monte_carlo.jl")
--- src/episode.jl
+++ src/episode.jl
@@ -5,6 +5,50 @@
 
 using Random
 
+"""
+    AbstractEpisode
+
+# Interface
+- `_start!`: This returns the initial state of the episode iteration and starts the environment and agent.
+- `_step!`: 
+- `_done`: 
+"""
+abstract type AbstractEpisode end
+
+function Base.iterate(ep::AbstractEpisode, state = _start!(ep))
+    _done(ep, state) && return nothing
+
+    s = state[1][1]
+    agent_ret = state[1][2]
+
+    s′, r, t, agent_ret = _step!(ep, agent_ret)
+
+    (s, agent_ret, s′, r, t), ((s′, agent_ret), state[2] + 1)
+end
+
+function run_episode!(episode::AbstractEpisode)
+    num_steps = 1
+    for sars ∈ ep
+        num_steps += 1
+    end
+    total_reward(episode), num_steps
+end
+
+function run_episode!(f::Base.Callable, episode::AbstractEpisode)
+    num_steps = 1
+    for sars ∈ ep
+        f(sars)
+        num_steps += 1
+    end
+    total_reward(total_reward), num_steps
+end
+
+run_episode!(f::Base.Callable, env::AbstractEnvironment, agent::AbstractAgent, args...) = 
+    run_episode!(f, Episode(env, agent, args...))
+
+run_episode!(env::AbstractEnvironment, agent::AbstractAgent, args...) =
+    run_episode!(ep, Episode(env, agent, args...))
+
 
 """
     Episode(env, agent, maxn, rng)
@@ -18,143 +62,74 @@ This is a struct for managing the components of an episode iterator. You should 
 - `rng=nothing`: The Random Number Generator for the episode (can either be nothing or an AbstractRNG)
 
 """
-mutable struct Episode{E<:AbstractEnvironment, A<:AbstractAgent, F<:AbstractFloat, AR}
+mutable struct Episode{E<:AbstractEnvironment, A<:AbstractAgent, F<:Number, AR}
+
     env::E
     agent::A
     total_reward::F # total reward of the episode
 
-    niter::Int      # current step in this episode
     maxn::Int       # max steps in an episode - should be constant during an episode
 
     rng::AR
 end
 
-Episode(env, agent, maxn=0, rng=nothing) =
-    Episode(env, agent, get_reward(env), 0, maxn, rng)
+Episode(env, agent, maxn=typemax(Int), rng=nothing) =
+    Episode{:default}(env, agent, get_reward(env), maxn, rng)
+
+_done(ep::Episode, state) =
+    state[2] > ep.maxn || is_terminal(ep.env)
+
+_get_action(action) = action
+_get_action(agent_ret::NamedTuple) = agent_ret.action
+
+_env_step!(ep::Episode{<:AbstractEnvironment, <:AbstractAgent, <:Number, <:AbstractRNG}, action) =
+    step!(ep.env, action, ep.rng)
+_env_step!(ep::Episode{<:AbstractEnvironment, <:AbstractAgent, <:Number, Nothing}, action) =
+    step!(ep.env, action)
+_agent_step!(ep::Episode{<:AbstractEnvironment, <:AbstractAgent, <:Number, <:AbstractRNG}, s′, r, t) =
+    step!(ep.agent, s′, r, t, ep.rng)
+_agent_step!(ep::Episode{<:AbstractEnvironment, <:AbstractAgent, <:Number, Nothing}, s′, r, t) =
+    step!(ep.agent, s′, r, t)
 
 function _start!(ep::Episode)
-    if ep.rng isa Nothing
+    s, agent_ret = if ep.rng isa Nothing
         s = start!(ep.env)
         agent_ret = start!(ep.agent, s)
-        
-        ep.total_reward = zero(ep.total_reward)
-        ep.niter = 1
-        return (s, agent_ret), ep.niter
+        s, agent_ret
     else
         s = start!(ep.env, ep.rng)
         agent_ret = start!(ep.agent, s, ep.rng)
-        
-        ep.total_reward = zero(ep.total_reward)
-        ep.niter = 1
-        return (s, agent_ret), ep.niter
+        s, agent_ret
     end
+
+    ep.total_reward = zero(ep.total_reward)
+    ep.niter = 1
+    return (s, agent_ret), ep.niter
 end
 
-_done(ep::Episode, state) =
-  (ep.maxn != 0 && ep.niter >= ep.maxn) || is_terminal(ep.env)
+function _step!(ep::Episode, agent_ret)
+    action = _get_action(agent_ret)
 
-function Base.iterate(ep::Episode, state = _start!(ep))
-    _done(ep, state) && return nothing
-
-    env = ep.env
-    agent = ep.agent
-
-    s = state[1][1]
-    agent_ret = state[1][2]
-
-    action = if agent_ret isa NamedTuple
-        agent_ret.action
-    else
-        agent_ret
-    end
-
-    s′, r, t = if ep.rng isa Nothing
-        step!(env, action)
-    else
-        step!(env, action, ep.rng)
-    end
-
-    agent_ret = if ep.rng isa Nothing
-        step!(agent, s′, r, t)
-    else
-        step!(agent, s′, r, t, ep.rng)
-    end
+    s′, r, t = _env_step!(ep, action)
+    agent_ret = _agent_step!(ep, s′, r, t)
 
     ep.total_reward += r
-    ep.niter = state[2]
-
-    (s, agent_ret, s′, r, t), ((s′, agent_ret), ep.niter + 1)
-end
-
-function run_episode!(f::Base.Callable, env::AbstractEnvironment, agent::AbstractAgent, args...)
-  ep = Episode(env, agent, args...)
-  for sars in ep
-    f(sars)
-  end
-  ep.total_reward, ep.niter
+    
+    s′, r, t, agent_ret
 end
 
 
-# function episode!(env, agent, max_eps_steps, total_steps, max_steps; callback=nothing, rng::Random.AbstractRNG=nothing)
-#     terminal = false
+mutable struct DispatchEpisode{T<:Val, E<:AbstractEpisode} <: AbstractEpisode
 
-#     # Start Environment
-#     s_t = if rng isa Nothing
-#         start!(env)
-#     else
-#         start!(env, rng)
-#     end
+    dispatch_type::T
+    episode::E
+    
+end
 
-#     # Start Agent
-#     agent_ret = if rng isa Nothing
-#         start!(agent, s_t)
-#     else
-#         start!(agent, s_t, rng)
-#     end
+DispatchEpisode(dispatch, episode::Episode) = DispatchEpisode(Val(dispatch), episode)
+DispatchEpisode(dispatch, args...) = DispatchEpisode(Val(dispatch), Episode(args...))
 
-#     total_rew = 0.0
-#     steps = 1
-#     if !(callback isa Nothing)
-#         callback(env, agent, (state=s_t,), agent_ret, steps)
-#     end
-
-#     if (total_steps+steps >= max_steps) # 5 Minutes of Gameplay = 18k steps.
-#         terminal = true
-#     end
-
-#     while !terminal
-#         # Figure out action!
-#         action = if agent_ret isa NamedTuple
-#             agent_ret.action
-#         else
-#             agent_ret
-#         end
-        
-#         # Step Environment
-#         s_tp1, rew, terminal = if rng isa Nothing
-#             step!(env, action)
-#         else
-#             step!(env, action, rng)
-#         end
-
-#         # Step Agent
-#         agent_ret = if rng isa Nothing
-#             step!(agent, s_tp1, rew, terminal)
-#         else
-#             step!(agent, s_tp1, rew, terminal, rng)
-#         end
-
-#         total_rew += rew
-#         steps += 1
-        
-#         if !(callback isa Nothing)
-#             callback(env, agent, (state=s_tp1, rew=rew, terminal=terminal), agent_ret, steps)
-#         end
-
-#         if (total_steps+steps >= max_steps) || (steps >= max_eps_steps) # 5 Minutes of Gameplay = 18k steps.
-#             break
-#         end
-#     end
-#     return total_rew, steps
-# end
-
+_start!(ep::DispatchEpisode) = _start!(ep.episode)
+_done(ep::DispatchEpisode, state) = _done(ep.episode, state)
+_step!(ep::DispatchEpisode, agent_ret) = _step!(ep.episode, agent_ret)
+Base.iterate(ep::DispatchEpisode, state = _start!(ep)) = Base.iterate(ep.episode, state)
